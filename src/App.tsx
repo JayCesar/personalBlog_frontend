d1cca722796fbb8d5d0db17a5222185503e20f9c
JayCesar <batistajulio215@gmail.com>
2023-07-25 13:48:20 -0300

    @JayCesar | Update: Add part 6 of documentation (route and tailwind)

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import Home from './paginas/home/Home';
 import './App.css';
-import React from 'react';
 
 function App() {
- 
   return (
     <>
-      <Home/>
+    <h1 className="text-3xl font-bold text-red-500 underline text-center">
+      Hello world!
+    </h1>
     </>
-  );
+);
 }
-
 export default App;
