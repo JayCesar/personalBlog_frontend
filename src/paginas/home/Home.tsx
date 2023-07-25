3f2d9395631cf850683f0899044371bcdb971ff2
JayCesar <batistajulio215@gmail.com>
2023-07-25 13:52:58 -0300

    @JayCesar | Update: Add grid explanation

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,16 +1,98 @@
-import React, { useState } from 'react';
+import React from 'react';
+import './Home.css';
+
+
 
 function Home() {
-  const [loggedIn, setLoggedIn] = useState(false);
+    return (
+        <>
 
-  return (
-    <div>
-      {loggedIn ? (
-        <h1>Bem-vindo de volta!</h1>
-      ) : (
-        <button onClick={() => setLoggedIn(true)}>Entrar</button>
-      )}
-    </div>
-  );
+            <div>
+                <div className='grid grid-cols-12 gap-2 m-4'>
+
+                    <div className='col-span-6 bg-green-300'>
+                        container 1
+                    </div>
+
+                    <div className='col-span-6 bg-yellow-200'>
+                        container 2
+                    </div>
+
+                </div>
+                <div className='grid grid-cols-12 gap-2 m-4'>
+
+                    <div className='col-span-8 bg-green-300'>
+                        container 1
+                    </div>
+
+                    <div className='col-span-4 bg-yellow-200'>
+                        container 2
+                    </div>
+
+                </div>
+
+                <div className='grid grid-cols-12 gap-2 m-4'>
+
+                    <div className='col-span-4 bg-green-300'>
+                        container 1
+                    </div>
+
+                    <div className='col-span-4 bg-yellow-200'>
+                        container 2
+                    </div>
+
+                    <div className='col-span-4 bg-red-500'>
+                        container 3
+                    </div>
+
+                </div>
+
+                <div className='grid grid-cols-12 gap-2 m-4'>
+
+                    <div className='col-span-3 bg-green-300'>
+                        container 1
+                    </div>
+
+                    <div className='col-span-3 bg-yellow-200'>
+                        container 2
+                    </div>
+
+                    <div className='col-span-3 bg-red-500'>
+                        container 3
+                    </div>
+
+                    <div className='col-span-3 bg-blue-500'>
+                        container 4
+                    </div>
+
+                </div>
+
+                <div className='grid grid-cols-12 gap-2 m-4'>
+
+                    <div className='col-span-4 bg-green-300'>
+                        container 1
+                    </div>
+
+                    <div className='col-span-4 bg-yellow-200'>
+                        container 2
+                    </div>
+
+                    <div className='col-span-2 bg-red-500'>
+                        container 3
+                    </div>
+
+                    <div className='col-span-2 bg-blue-500'>
+                        container 4
+                    </div>
+
+                </div>
+
+            </div>
+
+
+
+        </>
+    );
 }
+
 export default Home;
