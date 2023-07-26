06e842f38aad436f349cdf90a439ea3532a0e5f8
JayCesar <batistajulio215@gmail.com>
2023-07-26 09:31:58 -0300

    @JayCesar | Update: add part 11

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Home from './paginas/home/Home';
-import Navbar from './components/navbar/Navbar';
-import Footer from './components/footer/Footer';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Login from './paginas/login/Login';
+
+interface User {
+  id: number;
+  name: string;
+}
 
 function App() {
+
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    axios.get<User[]>('https://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        setUsers(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
   return (
-    <>
-    <BrowserRouter>
-        <Navbar />
-          <div className='min-h-[80vh]'>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/home" element={<Home />} />
-            </Routes>
-          </div>
-          <Footer />
-        </BrowserRouter>
-    </>
-);
+    <div>
+      <h1>Lista de usuários</h1>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
+
 export default App;
