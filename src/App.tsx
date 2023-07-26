67e26a530b9adc1b587ff034bfb08e82e3f374dd
JayCesar <batistajulio215@gmail.com>
2023-07-26 10:01:04 -0300

    @JayCesar | Update: add part 12

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,24 @@
-import './App.css';
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
+import UserProvider from './contexts/UserContext';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './paginas/home/Home';
+import Login from './paginas/login/Login';
 
-interface User {
-  id: number;
-  name: string;
-}
 
 function App() {
 
-  const [users, setUsers] = useState<User[]>([]);
-
-  useEffect(() => {
-    axios.get<User[]>('https://jsonplaceholder.typicode.com/users')
-      .then(response => {
-        setUsers(response.data);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }, []);
-
   return (
-    <div>
-      <h1>Lista de usuários</h1>
-      <ul>
-        {users.map(user => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
-    </div>
+    <UserProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+
+      </BrowserRouter>
+
+    </UserProvider>
   );
 }
 
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,31 +1,24 @@
-import React from 'react';
-// import homeLogo from '../../assets/home.png'
-import './Home.css';
+import React, { useContext } from 'react';
+
+import { UserContext } from '../../contexts/UserContext';
+import { Link, useNavigate } from 'react-router-dom';
 
 function Home() {
-    return (
-        <>
-        <div className="bg-indigo-900 flex justify-center">
-          <div className='container grid grid-cols-2 text-white'>
-            <div className="flex flex-col gap-4 items-center justify-center py-4">
-              <h2 className='text-5xl font-bold'>Seja bem vinde!</h2>
-              <p className='text-xl'>Expresse aqui seus pensamentos e opniões</p>
-  
-              <div className="flex justify-around gap-4">
-              
-                <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
-              </div>
-            </div>
-  
-            <div className="flex justify-center bg-black">
-              {/* <img src={homeLogo} alt="" className='w-2/3' /> */}
-      
-            </div>
-          </div>
-        </div>
-      
-      </>
-    );
+  const { nome, setNome } = useContext(UserContext);
+
+  return (
+    <div className='flex justify-center items-center'>
+      <div>
+        <h2 className="text-slate-900 text-5xl  my-4">Logar</h2>
+        <h2 className="text-slate-900 text-4xl ">Ola user : {nome}</h2>
+        <Link to="/login" className="my-4 rounded bg-indigo-400
+         hover:bg-indigo-900 text-white w-1/2 py-2 flex justify-center">
+          Voltar 
+        </Link>
+      </div>
+
+    </div>
+  );
 }
 
 export default Home;
