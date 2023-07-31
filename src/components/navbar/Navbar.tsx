925a7dec8062d4770539b53a729c98987790fe02
JayCesar <batistajulio215@gmail.com>
2023-07-31 18:22:32 -0300

    @JayCesar | Update: add part 21

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -27,9 +27,9 @@ function Navbar() {
             <div className='flex gap-4'>
             <div className='hover:underline'>Postagens</div>
             <Link to='/temas' className='hover:underline'>Temas</Link>
-              <div className='hover:underline'>Cadastrar tema</div>
+            <Link to='/cadastroTema'className='hover:underline'>Cadastrar tema</Link>
               <div className='hover:underline'>Perfil</div>
-              <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
+            <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
              
             </div>
           </div>
