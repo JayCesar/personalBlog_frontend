4f8487c9f58bd8f39f6970e5b3eaab73eae7bda6
JayCesar <batistajulio215@gmail.com>
2023-08-03 15:51:43 -0300

    @JayCesar | Add part  26

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,6 @@ import Home from './pages/home/Home';
 import { AuthProvider } from './contexts/AuthContext';
 import ListaTemas from './components/temas/listaTemas/ListaTemas';
 import FormularioTema from './components/temas/formularioTema/FormularioTema';
-import DeletarTema from './components/temas/deletarTema/DeletarTema';
-import ListaPostagens from './components/postagens/listaPostagens/ListaPostagens';
 
 
 function App() {
@@ -29,8 +27,7 @@ function App() {
               <Route path="/temas" element={<ListaTemas />} />
               <Route path="/cadastroTema" element={<FormularioTema />} />
               <Route path="/editarTema/:id" element={<FormularioTema />} />
-              <Route path="/deletarTema/:id" element={<DeletarTema />} />
-              <Route path="/postagens" element={<ListaPostagens />} />
+
             </Routes>
           </div>
           <Footer />
