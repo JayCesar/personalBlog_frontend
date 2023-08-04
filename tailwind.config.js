8bd400bfa8e96802f1e301a7fe331bb62a272598
JayCesar <batistajulio215@gmail.com>
2023-08-04 15:04:20 -0300

    @JayCesar | Update: add part 30

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        yelloy_01: '#F2CB05',
+        yelloy_02: '#F2B705',
+        oranga_01: '#F29F05',
+        orange_02: '#F27405',
+        orange_03: '#D95204',
+      }
+    },
   },
   plugins: [],
 }
