ae46b0684ff8cd97d9f2d7de53f9abf3dfdbd998
JayCesar <batistajulio215@gmail.com>
2023-08-04 19:55:45 -0300

    @JayCesar | Update: changes the home

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,11 +1,16 @@
-import React, { ChangeEvent, useContext, useEffect, useState } from 'react';
-import './Login.css';
+import React, { ChangeEvent, useContext, useEffect, useState } from "react";
+import "./Login.css";
 
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
 
-import { AuthContext } from '../../contexts/AuthContext';
-import UsuarioLogin from '../../models/UsuarioLogin';
-import { RotatingLines } from 'react-loader-spinner';
+import { AuthContext } from "../../contexts/AuthContext";
+import UsuarioLogin from "../../models/UsuarioLogin";
+import { RotatingLines } from "react-loader-spinner";
+import jayLogo from '../../assets/jayBlogLogo.svg'
+
+/* Test */
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon } from "@fortawesome/free-solid-svg-icons";
 
 function Login() {
   let navigate = useNavigate();
@@ -16,75 +21,119 @@ function Login() {
 
   const { usuario, handleLogin } = useContext(AuthContext);
 
-  const {isLoading} = useContext(AuthContext) 
+  const { isLoading } = useContext(AuthContext);
 
   useEffect(() => {
     if (usuario.token !== "") {
-        navigate('/home')
+      navigate("/home");
     }
-}, [usuario])
+  }, [usuario]);
 
-function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
-  setUsuarioLogin({
+  function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
+    setUsuarioLogin({
       ...usuarioLogin,
-      [e.target.name]: e.target.value
-  })
-}
+      [e.target.name]: e.target.value,
+    });
+  }
 
-function login(e: ChangeEvent<HTMLFormElement>) {
-  e.preventDefault()
-  handleLogin(usuarioLogin)
-}
+  function login(e: ChangeEvent<HTMLFormElement>) {
+    e.preventDefault();
+    handleLogin(usuarioLogin);
+  }
+
+  /* Tests */
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
+  const handleThemeSwitch = () => {
+    setTheme(theme === "dark" ? "ligth" : "dark");
+    console.log("test");
+  };
 
   return (
     <>
-      <div className="grid grid-cols-1 lg:grid-cols-2 h-screen place-items-center font-bold ">
-        <form className="flex justify-center items-center flex-col w-1/2 gap-4" onSubmit={login}>
-          <h2 className="text-slate-900 text-5xl ">Entrar</h2>
-          <div className="flex flex-col w-full">
+      <div className="bg-black_01 grid grid-cols-1 lg:grid-cols-2 h-screen place-items-center font-bold">
+        <div className="flex bg-slate-100 border-[1px] p-6 lg:p-10 rounded-[3%] shadow-lg shadow-black-500/50 dark:bg-black_01">
+        <form
+          className="flex justify-center items-center flex-col gap-4 w-full"
+          onSubmit={login}
+        >
+          <img src={jayLogo} alt="" className='w-[150px] mb-2' />
+          <div className="flex flex-col w-full text-black">
             <label htmlFor="usuario">Usuário</label>
             <input
               type="text"
               id="usuario"
               name="usuario"
-              placeholder="Usuario"
-              className="border-2 border-slate-700 rounded p-2"
-              value={usuarioLogin.usuario} 
-              onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+              placeholder="user@gen.com"
+              className="w-[280px] md:w-[390px] lg:w-[400px] mt-2 border-2 border-slate-700 rounded p-2 ring-2 ring-inset ring-orange_03 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-yellow_02"
+              value={usuarioLogin.usuario}
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                atualizarEstado(e)
+              }
             />
           </div>
-          <div className="flex flex-col w-full">
+          <div className="flex flex-col w-full text-black">
             <label htmlFor="senha">Senha</label>
             <input
               type="password"
               id="senha"
               name="senha"
               placeholder="Senha"
-              className="border-2 border-slate-700 rounded p-2"
-              value={usuarioLogin.senha} 
-              onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+              className="w-[280px] md:w-[390px] lg:w-[400px] mt-2 border-2 border-slate-700 rounded p-2 ring-1 ring-inset ring-orange_03 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-yellow_01"
+              value={usuarioLogin.senha}
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                atualizarEstado(e)
+              }
             />
           </div>
-          <button  type='submit' className="rounded bg-indigo-400 hover:bg-indigo-900 text-white w-1/2 py-2 flex justify-center">
-           {isLoading ? <RotatingLines
-            strokeColor="white"
-            strokeWidth="5"
-            animationDuration="0.75"
-            width="24"
-            visible={true}
-          /> :
-            <span>Entrar</span>}
+          <button
+            type="submit"
+            className="shadow-lg shadow-black-500/50 rounded bg-orange_03 dark:bg-orange_03 hover:bg-orange_01 dark:hover:bg-orange_01 text-white w-1/2 py-2 flex justify-center"
+          >
+            {isLoading ? (
+              <RotatingLines
+                strokeColor="white"
+                strokeWidth="5"
+                animationDuration="0.75"
+                width="24"
+                visible={true}
+              />
+            ) : (
+              <span>Entrar</span>
+            )}
           </button>
 
-          <hr className="border-slate-800 w-full" />
+          <hr className="border-slate-800 w-full dark:border-white" /> 
 
-          <p>
-            Ainda não tem uma conta?{' '}
-            <Link to="/cadastro" className="text-indigo-800 hover:underline">
-              Cadastre-se
-            </Link>
-          </p>
+          <div className="flex flex-col items-center">
+            <p className="text-black dark:text-white">Ainda não tem uma conta?</p>
+            <Link to="/cadastro" className="text-orange_01 dark:text-orange_03 hover:underline hover:text-orange_03 dark:hover:text-orange_01">Cadastre-se</Link>
+          </div>
+
+          <hr className="border-slate-800 w-full dark:border-white" /> 
+
+          <div className="flex flex-col gap-y-2 items-center justify-cente">
+            <h1 className="text-black dark:text-white">Cor do fundo</h1>
+            <label
+              className="hover:bg-orange_03 dark:hover:bg-orange_01 shadow-lg shadow-black-500/50 bg-black dark:bg-white w-[40px] h-[40px] flex justify-center items-center rounded-[50%] cursor-pointer"
+              onClick={handleThemeSwitch}
+            >
+              <FontAwesomeIcon
+                icon={faMoon}
+                className="h-[30px] text-white dark:text-black"
+              />
+            </label>
+          </div>
         </form>
+        </div>
         <div className="fundoLogin hidden lg:block"></div>
       </div>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,16 @@ module.exports = {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
-        yelloy_01: '#F2CB05',
-        yelloy_02: '#F2B705',
-        oranga_01: '#F29F05',
+        yellow_01: '#F2CB05',
+        yellow_02: '#F2B705',
+        orange_01: '#F29F05',
         orange_02: '#F27405',
         orange_03: '#D95204',
+        black_01: '#140B33',
       }
     },
   },
